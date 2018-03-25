a748f9808b795b68f2f9eef42ea2b684fb01d99a
Andrew Schultz <aschult5@fakemail.com>
2018-03-25 09:00:04 -0700

    Add support for initialization.

--- README.md
+++ README.md
@@ -3,18 +3,15 @@ Backup snapper Btrfs snapshots to another partition/drive.
 
 ## Usage
 ```bash
-snapback.sh [-i] <subvol_path> <mounted_backup_UUID>
+snapback.sh <btrfs_subvol_path> <mounted_backup_UUID>
 ```
 
 #### Initial backup
-**NOT YET IMPLEMENTED**
 You must initialize a snapback repository ONCE per snapper config per UUID.
-```bash
-snapback.sh -i / 050e1e34-39e6-4072-a03e-ae0bf90ba13a
-```
+If snapback is unable to find a previous snapshot, it will create and send a new initial snapshot.
 
-#### Incremental backups
-Now that your drive has been initialized, snapback may backup incrementally. It will take advantage of btrfs's incremental backups to reduce load.
+#### Example
+Normally, snapback will take advantage of btrfs's incremental backups to reduce load.
 ```bash
 snapback.sh / 050e1e34-39e6-4072-a03e-ae0bf90ba13a
 ```
@@ -25,4 +22,4 @@ blkid -s UUID -o value /dev/sdX#
 ```
 
 ## How it works
-snapback looks for snapshots of `<subvol_path>` taken by snapper with `backup=<mounted_backup_UUID>` in the userdata. If it finds one, it assumes the backup still exists on that partition and sends an incremental snapshot to it. Backup snapshots' file names are formatted in UTC time.
+snapback looks for snapshots of `<subvol_path>` taken by snapper with `backup=<mounted_backup_UUID>` in the userdata. If it finds one, it assumes the backup still exists on that partition and tries to send an incremental snapshot to it. If it didn't find a previous snapshot for that UUID, it will send a complete snapshot, rather than an incremental one. Backup snapshots' file names are formatted in UTC time.
--- snapback.sh
+++ snapback.sh
@@ -2,34 +2,36 @@
 
 usage() {
 	echo "Usage:"
-	echo -e "\t$0 <subvol_path> <mounted_backup_UUID>"
+	echo -e "\t$0 [-i] <subvol_path> <mounted_backup_UUID>"
 }
 
 cleanup() {
 	echo "FAIL: Cleaning up '$CONFIG' by deleting newly created snapshot: $NEW"
+	echo "If btrfs could not send an incremental snapshot, modify/delete snapshot $OLD so the UUID is not found"
 	snapper -c $CONFIG delete $NEW
 	exit 2
 }
 
-if [ $# -ne 2 ]; then
+if [ "$#" != "2" ]; then
 	usage
 	exit 1
 fi
-
 SUBVOL=$1
-CONFIG=$(snapper list-configs | grep "$SUBVOL\s\+$" | cut -d'|' -f 1 | tr -d '[:space:]')
 UUID=$2
+CONFIG=$(snapper list-configs | grep "$SUBVOL\s\+$" | cut -d'|' -f 1 | tr -d '[:space:]')
+UTC_TS=$(date -u +%F_%H:%M:%S)
 
 # Extract snapshot number of previous backup
 OLD=$(snapper -c $CONFIG list -t single | grep "backup=${UUID}" | tail -n 1 | cut -d'|' -f 1 | tr -d '[:space:]')
 if [ -z "$OLD" ]; then
-	echo "FAIL: No existing backup for UUID=${UUID}"
-	exit 2
+	echo "No previous snapshot found with userdata backup=${UUID}"
+else
+	echo "Found previous snapshot ${OLD} with userdata backup=${UUID}"
 fi
 
 # Create new snapshot
 echo "Creating snapshot for config '$CONFIG'"
-if ! NEW=$(snapper -c $CONFIG create -c number -d "backup $(date -u +%F)" -u "important=yes,backup=${UUID}" -p); then
+if ! NEW=$(snapper -c $CONFIG create -c number -d "backup $UTC_TS" -u "important=yes,backup=${UUID}" -p); then
 	echo "FAIL: Could not create snapshot for config '$CONFIG'"
 	exit 2
 fi
@@ -42,14 +44,18 @@ if [ ! -d ${DEST} ]; then
 	mkdir -p ${DEST}
 fi
 
-# Send the incremental snapshot
-echo "Sending incremental snapshot to ${DEST}"
-btrfs send -q -p ${SUBVOL}/.snapshots/${OLD}/snapshot ${SUBVOL}/.snapshots/${NEW}/snapshot | btrfs receive ${DEST} >/dev/null || cleanup
+# Send the snapshot
+if [ -z "$OLD" ]; then
+	echo "Sending snapshot to ${DEST}"
+	btrfs send -q ${SUBVOL}/.snapshots/${NEW}/snapshot | btrfs receive ${DEST} >/dev/null || cleanup
+else
+	echo "Sending incremental snapshot to ${DEST}"
+	btrfs send -q -p ${SUBVOL}/.snapshots/${OLD}/snapshot ${SUBVOL}/.snapshots/${NEW}/snapshot | btrfs receive ${DEST} >/dev/null || cleanup
+	# Remove backup tag from old snapshot
+	snapper -c $CONFIG modify -u "backup=" $OLD
+fi
 
 # Timestamp the new snapshot
-NAME=$(date -u +%F_%H:%M:%S)
-mv ${DEST}/snapshot ${DEST}/${NAME}
-echo "Created incremental snapshot ${NAME}"
 
-# Remove backup tag from old snapshot
-snapper -c $CONFIG modify -u "backup=" $OLD
+mv ${DEST}/snapshot ${DEST}/${UTC_TS}
+echo "Done sending snapshot ${UTC_TS}"
